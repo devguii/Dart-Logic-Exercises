e59c85f839d12f998ac564850da57555433a1036
gui <guigon@alunos.utfpr.edu.br>
2023-04-19 13:11:06 -0300

    feat: add exercise 2

--- exercices.dart
+++ exercices.dart
@@ -6,15 +6,25 @@
 // and created a separate function for each corresponding solution.
 
 main() {
-  /* 1 -Write a Dart function that takes an integer as input and returns
+  /* 1 - Write a Dart function that takes an integer as input and returns
    true if the integer is a prime number, false otherwise. */
 
   // answer:
+  print("\nQuestion 1 Answers: ");
   print(question1(10));
   print(question1(2));
   print(question1(-1));
   print(question1(0));
   print(question1(883));
+
+  /* 2 - Write a Dart function that takes a list of integers as input and 
+  returns the largest number in the list.
+  */
+
+  // answer:
+  print("\nQuestion 2 Answers: ");
+  print(question2([1, 5, 87, 90]));
+  print(question2([-1, -5, -87, -90]));
 }
 
 bool question1(int a) {
@@ -28,4 +38,15 @@ bool question1(int a) {
     }
   }
   return true;
+}
+
+int question2(List<int> integers) {
+  int largest = integers[0];
+
+  for (int number in integers) {
+    if (number > largest) {
+      largest = number;
+    }
+  }
+  return largest;
 }
